40f6c718d3a2416ad5b2c2f4d7a31c155f304491
kcn3388 <kcn3388@126.com>
2023-05-29 09:41:02 +0800

    remove auto pass

--- query.py
+++ query.py
@@ -1,10 +1,11 @@
-import traceback
 import asyncio
+import traceback
 from asyncio import Lock
-from json import load, loads
+from json import load
 from os.path import dirname, join
+
 from nonebot import get_bot, on_command
-from hoshino.aiorequests import get
+
 from .geetest import public_address
 from .pcrclient import pcrclient, ApiException, bsdkclient
 
@@ -57,40 +58,40 @@ async def captchaVerifier(*args):
         validating = False
         return validate
 
-    while captcha_cnt < 5:
-        captcha_cnt += 1
-        try:
-            print(f'测试新版自动过码中，当前尝试第{captcha_cnt}次。')
-
-            await asyncio.sleep(1)
-            uuid = loads(await (await get(url="https://pcrd.tencentbot.top/geetest")).content)["uuid"]
-            print(f'uuid={uuid}')
-
-            ccnt = 0
-            while ccnt < 3:
-                ccnt += 1
-                await asyncio.sleep(5)
-                res = await (await get(url=f"https://pcrd.tencentbot.top/check/{uuid}")).content
-                res = loads(res)
-                if "queue_num" in res:
-                    nu = res["queue_num"]
-                    print(f"queue_num={nu}")
-                    tim = min(int(nu), 3) * 5
-                    print(f"sleep={tim}")
-                    await asyncio.sleep(tim)
-                else:
-                    info = res["info"]
-                    if info in ["fail", "url invalid"]:
-                        break
-                    elif info == "in running":
-                        await asyncio.sleep(5)
-                    else:
-                        print(f'info={info}')
-                        validating = False
-                        return info
-        except:
-            pass
-
+    # while captcha_cnt < 5:
+    #     captcha_cnt += 1
+    #     try:
+    #         print(f'测试新版自动过码中，当前尝试第{captcha_cnt}次。')
+    #
+    #         await asyncio.sleep(1)
+    #         uuid = loads(await (await get(url="https://pcrd.tencentbot.top/geetest")).content)["uuid"]
+    #         print(f'uuid={uuid}')
+    #
+    #         ccnt = 0
+    #         while ccnt < 3:
+    #             ccnt += 1
+    #             await asyncio.sleep(5)
+    #             res = await (await get(url=f"https://pcrd.tencentbot.top/check/{uuid}")).content
+    #             res = loads(res)
+    #             if "queue_num" in res:
+    #                 nu = res["queue_num"]
+    #                 print(f"queue_num={nu}")
+    #                 tim = min(int(nu), 3) * 5
+    #                 print(f"sleep={tim}")
+    #                 await asyncio.sleep(tim)
+    #             else:
+    #                 info = res["info"]
+    #                 if info in ["fail", "url invalid"]:
+    #                     break
+    #                 elif info == "in running":
+    #                     await asyncio.sleep(5)
+    #                 else:
+    #                     print(f'info={info}')
+    #                     validating = False
+    #                     return info
+    #     except:
+    #         pass
+    captcha_cnt = 5
     if captcha_cnt >= 5:
         otto = False
         await bot.send_private_msg(user_id=acinfo[0]['admin'],
